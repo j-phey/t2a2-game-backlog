b7606f5590eccca563b104c92345faf893436678
jjjjjjpppppp <jonphey91@gmail.com>
2023-12-11 16:29:27 +1100

    Created currently_playing routes - single entry read, update, delete

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,7 +1,13 @@
 from controllers.games_controller import games
 from controllers.auth_controller import auth
+from controllers.currently_playing_controller import currently_playing
+# from controllers.backlog_controllercontroller import 
+# from controllers.wishlist_controller_controller import 
 
 registerable_controllers = [
     auth,
-    games
+    games,
+    currently_playing
+    # backlog
+    # wishlist
 ]
--- src/controllers/currently_playing_controller.py
+++ src/controllers/currently_playing_controller.py
@@ -14,7 +14,7 @@ currently_playing = Blueprint('currently_playing', __name__, url_prefix="/curren
 
 @currently_playing.route("/", methods=["GET"])
 def get_currently_playing():
-    # Get all the currently playing games from the database
+    # Get all the currently playing entries from the database
     stmt = db.select(CurrentlyPlaying)
     currently_playing = db.session.scalars(stmt)
     # Converting the entries from the database into a JSON format and storing them in 'result'
@@ -22,12 +22,26 @@ def get_currently_playing():
     # Returning the result in JSON format
     return jsonify(result)
 
+# Route: GET a single currently playing 
+@currently_playing.route("/<int:id>/", methods=["GET"])
+def get_single_currently_playing(id):
+    stmt = db.select(CurrentlyPlaying).filter_by(id=id)
+    currently_playing = db.session.scalar(stmt)
+    # Returns an error if the entry doesn't exist
+    if not currently_playing:
+        return abort(400, description= "Game does not exist")
+    # Convert the currently_playings from the database into a JSON format and store them in result
+    result = currently_playing_single_schema.dump(currently_playing)
+    # return the data in JSON format
+    return jsonify(result)
+
+
 #  Route: POST Create a new currently playing entry
 
 @currently_playing.route("/", methods=["POST"])
 @jwt_required()
 def currently_playing_create():
-    #Create a game entry
+    #Create a currently_playing entry
     currently_playing_fields = currently_playing_single_schema.load(request.json)
 
     new_currently_playing = CurrentlyPlaying()
@@ -37,5 +51,71 @@ def currently_playing_create():
     # Add to the database and commit
     db.session.add(new_currently_playing)
     db.session.commit()
-    # Return the game in the response
+    # Return the currently_playing in the response
     return jsonify(currently_playing_single_schema.dump(new_currently_playing))
+
+
+# Route: Update a currently playing entry
+ 
+@currently_playing.route("/<int:id>/", methods=["PUT"])
+@jwt_required()
+def update_currently_playing(id):
+    # Create a new currently_playing
+    currently_playing_fields = currently_playing_single_schema.load(request.json)
+
+    # Get the user id by invoking get_jwt_identity
+    user_id = get_jwt_identity()
+    # Find the user in the db
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    # Make sure the user is in the database
+    if not user:
+        return abort(401, description="Invalid user")
+    # Stop the request if the user is not an admin
+    if not user.admin:
+        return abort(401, description="Unauthorised user")
+    # Find the entry
+    stmt = db.select(CurrentlyPlaying).filter_by(id=id)
+    currently_playing = db.session.scalar(stmt)
+    # Return an error if the currently_playing doesn't exist
+    if not currently_playing:
+        return abort(400, description= "Game does not exist")
+    # Update the currently_playing with the given values
+    currently_playing.progress = currently_playing_fields["progress"]
+    currently_playing.date_added = date.today()
+    # Add to the database and commit
+    db.session.commit()
+    # Return the currently_playing in the response
+    return jsonify(currently_playing_single_schema.dump(currently_playing))
+
+
+# Route: Delete a currently playing entry
+
+# /<int:id> lets the server know what currently playing entry we want to delete 
+@currently_playing.route("/<int:id>", methods=["DELETE"])
+@jwt_required()
+# Include the id parameter
+def currently_playing_delete(id):
+    # Get the user id by invoking get_jwt_identity()
+    user_id = get_jwt_identity()
+    # Find the user in the db
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    # Make sure the user is in the database
+    if not user:
+        return abort(401, description="Invalid user")
+    # Prevents the request if the user is not an admin
+    if not user.admin:
+        return abort(401, description="Unauthorised user")
+
+    # Find the currently_playing id
+    stmt = db.select(CurrentlyPlaying).filter_by(id=id)
+    currently_playing = db.session.scalar(stmt)
+    # Return an error if the currently_playing id doesn't exist
+    if not currently_playing:
+        return abort(400, description= "Game doesn't exist")
+    #D elete the currently_playing from the database and commit the change
+    db.session.delete(currently_playing)
+    db.session.commit()
+    # Return the currently_playing as the response
+    return jsonify(currently_playing_single_schema.dump(currently_playing))
